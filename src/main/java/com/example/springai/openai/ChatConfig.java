71241fa6aff58df3a764c6c12e2dcc550866ba71
0ys <0ysgong@gmail.com>
2025-08-06 09:50:37 +0900

    baseURL 지정 방식

--- src/main/java/com/example/springai/openai/ChatConfig.java
+++ src/main/java/com/example/springai/openai/ChatConfig.java
@@ -1,16 +1,11 @@
 package com.example.springai.openai;
 
-import org.springframework.ai.model.SimpleApiKey;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ChatConfig {
@@ -30,18 +25,8 @@ public class ChatConfig {
     public OpenAiChatModel openAiChatModel() {
         OpenAiApi openAiApi = OpenAiApi.builder()
                 .apiKey(apiKey)
+                .baseUrl(baseUrl)
                 .build();
-
-//        OpenAiApi openAiApi = new OpenAiApi(
-//                baseUrl,
-//                new SimpleApiKey(apiKey),
-//                new LinkedMultiValueMap<>(),
-//                "/chat/completions",
-//                "/embeddings",
-//                RestClient.builder(),
-//                WebClient.builder(),
-//                new DefaultResponseErrorHandler()
-//        );
 
         OpenAiChatOptions openAiChatOptions = OpenAiChatOptions.builder()
                 .model(model)
